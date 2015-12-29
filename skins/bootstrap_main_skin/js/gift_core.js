dcb4741d91b5dd1fa5ad0339911f652ce606f424
SergeyU1986 <sergeyU1986@outlook.com>
2015-12-29 08:30:15 -0600

    Sergey's first codding

--- skins/bootstrap_main_skin/js/gift_core.js
+++ skins/bootstrap_main_skin/js/gift_core.js
@@ -1,5 +1,6 @@
 /**
  *  Sergey's gift div sliping
+ *  Using jQuery and CSS
  */
 
 jQuery(document).ready(function(){
@@ -7,12 +8,11 @@ jQuery(document).ready(function(){
 	$("#sergey_gift_div .front").css("visibility","visible");
 	$("#sergey_gift_div .back").css("visibility","hidden");
 	$("#sergey_gift_div .front").addClass("flipper-front");
-	$("#sergey_gift_div").click(function(){
+	$("#sergey_gift_div").click( function(){
 		var obj = $(this).children().get(0);
 		$("#sergey_gift_div .front").css("visibility","hidden");
 		$("#sergey_gift_div .back").css("visibility","hidden");
-		if($("#sergey_gift_div .front").hasClass("flipper-front")){
-			
+		if( $("#sergey_gift_div .front").hasClass("flipper-front") ){
 			$("#sergey_gift_div .front").removeClass("flipper-front");
 			$("#sergey_gift_div .front").addClass("flipper-back");
 			$("#sergey_gift_div .front").css("visibility","hidden");
@@ -20,7 +20,6 @@ jQuery(document).ready(function(){
 			$("#sergey_gift_div .back").removeClass("flipper-back");			
 			$("#sergey_gift_div .back").addClass("flipper-front");			
 		}else{
-			
 			$("#sergey_gift_div .front").removeClass("flipper-back");
 			$("#sergey_gift_div .front").addClass("flipper-front");
 			$("#sergey_gift_div .front").css("visibility","visible");
